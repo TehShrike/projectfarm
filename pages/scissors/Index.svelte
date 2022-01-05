225cafed197e71e437c8dfaeac083399163974f8
Josh Duff <me@JoshDuff.com>
2022-01-05 16:27:15 -0600

    add a note about sharpness numbers

--- pages/scissors/Index.svelte
+++ pages/scissors/Index.svelte
@@ -31,6 +31,10 @@
 	Finally we'll sharpen some scissors after they become really dull from all the testing.
 </p>
 
+<p>
+	<strong>Sharpness numbers:</strong> for context, a double edge razor blade is around 50, a utility blade is anywhere from 150 to 200, and a new high-end cutlery edge is anywhere from 250 to 350.
+</p>
+
 <TableHolder>
 	<Table {values} {columns} {identifier} />
 </TableHolder>
